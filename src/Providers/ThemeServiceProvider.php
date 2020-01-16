04b4639ab65d019cbefe71f0393be53ae569e56a
FinestTrachten <59439750+FinestTrachten@users.noreply.github.com>
2020-01-16 12:50:38 +0100

    ..

--- src/Providers/ThemeServiceProvider.php
+++ src/Providers/ThemeServiceProvider.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace Theme\Providers;
+namespace FTLegendCustom\Providers;
 
 use IO\Extensions\Functions\Partial;
 use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\ServiceProvider;
 use Plenty\Plugin\Templates\Twig;
 
-class ThemeServiceProvider extends ServiceProvider
+class FTLegendCustomServiceProvider extends ServiceProvider
 {
 
   const PRIORITY = 98;
